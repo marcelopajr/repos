b9e2ab2e17a32251f559348b3704df4b01fc123b
Marcelo Pereira <marcelo96jr@gmail.com>
2021-09-13 21:58:45 -0300

    List of repositories

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,7 +1,7 @@
 import api from '../../services/api';
 import { useState, useCallback } from 'react';
-import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
-import { Container, Form, SubmitButton } from './styles';
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
+import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
 
 export default function Main() {
   const [newRepo, setNewRepo] = useState('');
@@ -36,6 +36,14 @@ export default function Main() {
     setNewRepo(e.target.value);
   }
 
+  const handleDelete = useCallback(
+    (repo) => {
+      const find = repositories.filter((r) => r.name !== repo);
+      setRepositories(find);
+    },
+    [repositories],
+  );
+
   return (
     <Container>
       <h1>
@@ -58,6 +66,22 @@ export default function Main() {
           )}
         </SubmitButton>
       </Form>
+
+      <List>
+        {repositories.map((repo) => (
+          <li key={repo.name}>
+            <span>
+              <DeleteButton onClick={() => handleDelete(repo.name)}>
+                <FaTrash size={14} />
+              </DeleteButton>
+              {repo.name}
+            </span>
+            <a href="/">
+              <FaBars size={20} />
+            </a>
+          </li>
+        ))}
+      </List>
     </Container>
   );
 }
