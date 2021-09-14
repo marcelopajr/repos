ec4abd8d2210564afa9930fafd2975ce17b0cbef
Marcelo Pereira <marcelo96jr@gmail.com>
2021-09-13 21:43:17 -0300

    Getting repos

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,7 +1,41 @@
-import { FaGithub, FaPlus } from 'react-icons/fa';
+import api from '../../services/api';
+import { useState, useCallback } from 'react';
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      async function submit() {
+        setLoading(true);
+
+        try {
+          const response = await api.get(`repos/${newRepo}`);
+          const data = { name: response.data.full_name };
+          setRepositories([...repositories, data]);
+          setNewRepo('');
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
+      }
+
+      submit();
+    },
+    [newRepo, repositories],
+  );
+
+  function handleInputChange(e) {
+    setNewRepo(e.target.value);
+  }
+
   return (
     <Container>
       <h1>
@@ -9,10 +43,19 @@ export default function Main() {
         My repositories
       </h1>
 
-      <Form>
-        <input onSubmit={() => {}} type="text" placeholder="Add repositories" />
-        <SubmitButton>
-          <FaPlus color="#FFF" size={14} />
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add repositories"
+          value={newRepo}
+          onChange={handleInputChange}
+        />
+        <SubmitButton loading={loading ? 1 : 0}>
+          {loading ? (
+            <FaSpinner color="#FFF" size={14} />
+          ) : (
+            <FaPlus color="#FFF" size={14} />
+          )}
         </SubmitButton>
       </Form>
     </Container>
