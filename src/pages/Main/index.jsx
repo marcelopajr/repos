e17d5b3f29137d743a74d4802c47c2bdda7220c3
Marcelo Pereira <marcelo96jr@gmail.com>
2021-09-15 17:18:30 -0300

    Loading repo details

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,5 +1,6 @@
 import api from '../../services/api';
 import { useState, useEffect, useCallback } from 'react';
+import { Link } from 'react-router-dom';
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
 import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
 
@@ -105,9 +106,9 @@ export default function Main() {
               </DeleteButton>
               {repo.name}
             </span>
-            <a href="/">
+            <Link to={`/repo/${encodeURIComponent(repo.name)}`}>
               <FaBars size={20} />
-            </a>
+            </Link>
           </li>
         ))}
       </List>
--- src/pages/Repo/index.jsx
+++ src/pages/Repo/index.jsx
@@ -1,3 +1,54 @@
-export default function Repo() {
-  return <h1>Repo</h1>;
+import api from '../../services/api';
+import { useState, useEffect } from 'react';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Container, Owner, Loading, BackButton } from './styles';
+
+export default function Repo({ match }) {
+  const [repository, setRepository] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function load() {
+      const nameRepo = decodeURIComponent(match.params.repo);
+
+      const [repositoryData, issuesData] = await Promise.all([
+        api.get(`/repos/${nameRepo}`),
+        api.get(`/repos/${nameRepo}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5,
+          },
+        }),
+      ]);
+
+      setRepository(repositoryData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    }
+
+    load();
+  }, [match.params.repo]);
+
+  if (loading) {
+    return (
+      <Loading>
+        <h1>Loading...</h1>
+      </Loading>
+    );
+  }
+
+  return (
+    <Container>
+      <BackButton to="/">
+        <FaArrowLeft color="#000" size={30} />
+      </BackButton>
+
+      <Owner>
+        <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+        <h1>{repository.name}</h1>
+        <p>{repository.description}</p>
+      </Owner>
+    </Container>
+  );
 }
